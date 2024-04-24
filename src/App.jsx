602646a35fbfa6b6336bf510753ae7582791b4a4
Adarsh nm <adarshnm02@gmail.com>
2024-04-24 17:51:37 +0530

    Implement the Submition section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Todolist from './components/Todolist'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello World</h1>
-        
-    </>
+    <div>
+   <Todolist />
+    </div>
   )
 }
 
